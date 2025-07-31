0ea33e79717b3309df63891ad2d4a74483df39d0
chaudhary-aman <amanchaudharyy627@gmail.com>
2025-07-31 18:58:50 +0545

    4th

--- logic.js
+++ logic.js
@@ -1,6 +1,33 @@
-const answer=document.getElementById("display");
+var operation="";
+var 𝚿=100;
 
-function insert(key)
+function insert(varr)
 {
-    answer.value+=key;
+    operation+=varr;
+    document.getElementById("display").value=operation;
 }
+
+function erase()
+{
+    operation=operation.slice(0,-1);
+    document.getElementById("display").value=operation;
+}
+function clear()
+{
+    operation="";
+    document.getElementById("display").value=operation;
+}
+function calculation()
+{
+    try {
+        result=eval(operation);
+        document.getElementById("display").value=result;
+        operation=result.toString();
+
+    }
+    catch(e)
+    {
+        document.getElementById("display").value="Error";
+    }
+}
+
