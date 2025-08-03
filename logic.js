3a67ee754b2d3f35a9a91abac170d6257b420394
chaudhary-aman <amanchaudharyy627@gmail.com>
2025-08-03 23:28:22 +0545

    Js eventlistner

--- logic.js
+++ logic.js
@@ -33,3 +33,29 @@ function calculation()
     }
 }
 
+
+
+
+// Select all buttons inside the Numbers div
+const buttons = document.querySelectorAll('#Numbers button');
+
+buttons.forEach(button => {
+    // If button has data-value, insert that value on click
+    if (button.dataset.value) {
+        button.addEventListener('click', () => {
+            insert(button.dataset.value);
+        });
+    }
+    // If button has data-action, call corresponding function
+    else if (button.dataset.action) {
+        if (button.dataset.action === 'clear') {
+            button.addEventListener('click', clear);
+        } else if (button.dataset.action === 'calculate') {
+            button.addEventListener('click', calculation);
+        } else if (button.dataset.action === 'erase') {
+            button.addEventListener('click', erase);
+        }
+    }
+});
+
+
